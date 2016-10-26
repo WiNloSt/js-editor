21169ea63aee7534f0b90ce53e205627e7080f06
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-10-27 00:13:52 +0700

    Make JsEditor an NPM package

---
+++ config/webpack.config.lib.js
@@ -0,0 +1,14 @@
+const prodConfig = require('./webpack.config.prod.js')
+const path = require('path')
+
+const libConfig = Object.assign({}, prodConfig, {
+  entry: path.resolve('src','ExportEditorApp.js'),
+  output: Object.assign({}, prodConfig.output, {
+    path: path.resolve('lib'),
+    filename: 'JsEditor.js'
+  })
+})
+
+console.log(libConfig.output)
+
+module.exports = libConfig
