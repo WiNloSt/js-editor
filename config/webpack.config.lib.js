173ad40b14f06dc312ae6d11884419ef11da6f03
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-11-01 21:23:18 +0700

    Add style to package and bump version

--- config/webpack.config.lib.js
+++ config/webpack.config.lib.js
@@ -1,12 +1,15 @@
 const prodConfig = require('./webpack.config.prod.js')
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const libConfig = Object.assign({}, prodConfig, {
   entry: path.resolve('src','ExportEditorApp.js'),
   output: Object.assign({}, prodConfig.output, {
     path: path.resolve('lib'),
-    filename: 'JsEditor.js'
+    filename: 'index.js'
   })
 })
+
+libConfig.plugins[libConfig.plugins.length - 1] = new ExtractTextPlugin('style.css')
 
 module.exports = libConfig
